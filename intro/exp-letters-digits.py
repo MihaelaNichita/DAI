155242bf75af46e7b00c3a82f908187d5f156d04
MihaelaNichita <40822291+MihaelaNichita@users.noreply.github.com>
2019-03-01 09:16:07 +0200

    Add methods for second expression

--- intro/exp-letters-digits.py
+++ intro/exp-letters-digits.py
@@ -19,12 +19,20 @@ def getNumbers1():
 
 def getNumbers2():
     for F in range(10):
-        for O in range(10) and O!=F:
-            for R in range(10) and R not in [F,O]:
-                for T in range(10) and T not in [F,O,R]:
-                    for Y in range(10) and Y not in [F,O,R,T]:
-                        num = exp2(F,O,R,T,Y)
-                        printIfEqual2(num,F,O,R,T,Y)
+        for O in range(10):
+            if O!=F:
+                for R in range(10):
+                    if R not in [F,O]:
+                        for T in range(10):
+                            if T not in [F,O,R]:
+                                for Y in range(10):
+                                    if Y not in [F,O,R,T]:
+                                        for E in range(10):
+                                            if E not in [F,O,R,T,Y]:
+                                                for N in range(10):
+                                                    if N not in [F,O,R,T,Y,E]:
+                                                        num = exp2(F,O,R,T,Y,E,N)
+                                                        printIfEqual2(num,F,O,R,T,Y,E,N)
 
 
 def exp1(O,D):
@@ -38,16 +46,25 @@ def printIfEqual1(num,O,D):
     E = int(num/10)%10
     V = int(num/100)%10
     if E in [V,N,O,D] or N in [V,O,D] or V in [O,D] or E!=int(num/1000)%10:
-        pass
+        return
     else:
         print(f"{O}{D}{D}+{O}{D}{D}={E}{V}{E}{N}")
 
 
-def printIfEqual2(num,F,O,R,T,Y):
-    if int(num/100000) == 0:
+def printIfEqual2(num,F,O,R,T,Y,E,N):
+    if int(num/10000) == 0 or int(num/100000) !=0 or num%100 != T*10+Y:
         return
 
+    num = int(num/100)
+    X = num%10
+    I = int(num/10)%10
+    S = int(num/100)%10
 
-        
+    if S in [F,O,R,T,Y,E,N,I,X] or I in [F,O,R,T,Y,E,N,X] or X in [F,O,R,T,Y,E,N]:
+        return
+    print(f"{F}{O}{R}{T}{Y}+{T}{E}{N}+{T}{E}{N}={S}{I}{X}{T}{Y}")
+
+print("ODD + ODD = EVEN")      
 getNumbers1()
-
+print("FORTY + TEN + TEN = SIXTY") 
+getNumbers2()
